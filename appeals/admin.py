0b2868c9196108be06ca8f6797a22a468ab53d5b
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-11-27 12:35:19 +0300

    new repo

---
+++ appeals/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Appeal, Appealer, Donation, Tag
+# Register your models here.
+
+admin.site.register(Appealer)
+admin.site.register(Appeal)
+admin.site.register(Donation)
+admin.site.register(Tag)
